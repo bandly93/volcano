232c3b266182e12cfb7ea9f32e5449eb0f62cd61
steven <stevenle2011@gmail.com>
2017-08-25 14:22:25 -0700

    fix server

--- src/server/server.js
+++ src/server/server.js
@@ -4,13 +4,13 @@ var app = express();
 app.use(morgan('dev'));
 var port = process.env.PORT || 3000;
 var path = require('path');
-
+/*
 app.get('*.js', function (req, res, next) {
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
   next();
 });
-
+*/
 app.use(express.static(path.resolve(__dirname ,'../../dist')));
 app.use('/',express.static(path.resolve(__dirname ,'../client/public')));
 
