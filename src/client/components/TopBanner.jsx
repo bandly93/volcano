287c39478a18fdee4da97f3602b10478ff3ab600
steven <stevenle2011@gmail.com>
2017-08-29 08:38:16 -0700

    add banner

---
+++ src/client/components/TopBanner.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function TopBanner(){
+	return(
+		<img className = 'banner' src='https://i.imgur.com/N8TIgh6.jpg'/>
+	)
+}
+export default TopBanner
--- src/client/public/index.jsx
+++ src/client/public/index.jsx
@@ -1,11 +1,13 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from '../components/NavBar.jsx';
+import TopBanner from '../components/TopBanner.jsx';
 
 class Index extends Component{
 	render(){
 		return(
 			<div>
+				<TopBanner />
 				<NavBar />
 			</div>
 		)
