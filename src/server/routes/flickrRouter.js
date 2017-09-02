36fa47724fed78259b811e1e2aa77a96262d1efa
Band Ly <bandly@Bands-MacBook-Pro.local>
2017-09-02 10:43:20 -0700

    add new font

--- src/server/routes/flickrRouter.js
+++ src/server/routes/flickrRouter.js
@@ -1,10 +1,10 @@
 var express = require("express");
 var flickrRouter = express().Router;
 var axios = require(axios);
+import {} from 
 
 //testing purposes 
-var api_key = '647e979532eba1076860120f81f5cb57'
-var user_id = '151931971%40N04' //bandly1993
+
 
 var url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key='+ api_key + '&user_id=' + user_id +'&format=json&nojsoncallback=1&auth_token=72157685944084373-5b383c86c7c6a99b&api_sig=b8e516de890711f0595cedc11eda6437';
 
