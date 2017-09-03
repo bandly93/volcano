d7e4a833190333ed7768e01763fc3a72e2b6987c
Band Ly <bandly@Bands-MacBook-Pro.local>
2017-09-03 15:39:02 -0700

    change photosObj to photosArr

--- src/client/subViews/Portraits.jsx
+++ src/client/subViews/Portraits.jsx
@@ -1,18 +1,32 @@
 import React,{Component} from 'react'; 
 
 
+let photosArr = [
+	{
+		photo : 'https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg',
+		name : 'minions'
+	},
+	{
+		photo : 'http://www.unaids.org/sites/default/files/media/20170324_TBDay_PR_960.jpg',
+		name : 'girl'
+	}
+]
 
 class Portraits extends Component{
-	
 	render(){
-		let photosObj = {
-			'photo' : "https://www.cleverfiles.com/howto/wp-content/uploads/2016/08/mini.jpg",
-			'name' : "minions"
-		}
 		return(
-			<img 
-				src = {photosObj.photo} 
-				key = {photosObj.name} />
+			<div>
+				{
+					photosArr.map(photo => {
+						return (
+							<img 
+								className = "insta-photo"
+								src = {photo.photo} 
+								key = {photo.name} />
+						)
+					})
+				}
+			</div>
 		)
 	}
 }
