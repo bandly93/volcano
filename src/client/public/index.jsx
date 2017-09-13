233c99005ad17ca2d73c87338a808d1ee6506f30
steven <stevenle2011@gmail.com>
2017-09-12 17:28:30 -0700

    add landing page

--- src/client/public/index.jsx
+++ src/client/public/index.jsx
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from '../components/NavBar.jsx';
 import TopBanner from '../components/TopBanner.jsx';
+import Landing from '../views/Landing.jsx';
 import {Provider} from 'react-redux';
 import configureStore from '../redux/store';
 
@@ -10,8 +11,7 @@ class Index extends Component{
 	render(){
 		return(
 			<div>
-				<TopBanner />
-				<NavBar />
+				<Landing />
 			</div>
 		)
 	}
