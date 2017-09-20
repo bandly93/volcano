265def1b2e27b351c3fd407914a5d2c70357b78a
steven <stevenle2011@gmail.com>
2017-09-19 18:52:29 -0700

    add contact form

--- src/client/views/About.jsx
+++ src/client/views/About.jsx
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import { fetchData } from '../redux/modules/fetchThunk';
 import { flickrAct } from '../redux/modules/flickrModule';
 import { connect } from 'react-redux';
+import ContactForm from '../components/ContactForm.jsx';
 
 
 const lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
@@ -10,31 +11,30 @@ class About extends Component{
 	componentDidMount(){
 		this.props.fetchData('/flickr',this.props.flickrAct)
 	}
-	render(){
-		let list;
-		if(this.props.flickr.images){
-			list = this.props.flickr.images;
-			return(
-				<div className = "about-container">
-					<div className = "top-row">
-						<span>
-							<img key = {this.props.flickr.images[5].name} src = {this.props.flickr.images[5].url}/>
-							<p>{lorem_ipsum}</p>
-						</span>
-						<span>
-							<img key = {this.props.flickr.images[1].name} src = {this.props.flickr.images[1].url}/>
-							<p>{lorem_ipsum}</p>
-						</span>
-					</div>
+	list(){
+		return(
+			<div className = "about-container">
+				<div className = "top-row">
+					<span>
+						<img key = {this.props.flickr.images[5].name} src = {this.props.flickr.images[5].url}/>
+						<p>{lorem_ipsum}</p>
+					</span>
+					<span>
+						<img key = {this.props.flickr.images[1].name} src = {this.props.flickr.images[1].url}/>
+						<p>{lorem_ipsum}</p>
+					</span>
 				</div>
-			)
-		}else{
-			return (
-				<div>
-					{this.props.flickr.images? list: null}
-				</div>
-			)	
-		}
+			</div>
+		)
+	}
+	render(){	
+		return (
+			<div>
+				{this.props.flickr.images? this.list(): null}
+				<ContactForm />
+			</div>
+		)	
+		
 	}
 }
 
