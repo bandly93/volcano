75b42995bfeed87b73a556581b450303b0c1cbd3
lesteven <le_steven@outlook.com>
2017-10-12 21:10:03 -0700

    update dash-nav

--- src/client/adminViews/Dashboard.jsx
+++ src/client/adminViews/Dashboard.jsx
@@ -10,7 +10,7 @@ import PostBlog from './PostBlog.jsx';
 import Upload from './Upload.jsx';
 import UploadTest from "./TestingUpload.jsx";
 import DashHome from './DashHome.jsx';
-
+import ProfilePic from '../public/images/about/profile.jpg';
 
 class Dashboard extends Component{
     constructor(){
@@ -47,15 +47,12 @@ class Dashboard extends Component{
 			<Router>
 				<div className='dashboard'>
 
-                    <nav className='dash-monav'>
-                        <div className='profilePic'></div>
-                        <a className='hamburger' onClick={this.showMenu}></a>
-                        <h1 className='flex-space title'>Volcano Boyz</h1> 
-                        <a className='home-icon'onClick={this.goHome} ></a> 
-                    </nav>
-                    <div className='dash-desk'>
-					<nav className='dash-nav'  
+					<nav className='dash-sidenav'  
                         style={{display:this.state.display}}>
+                        <div className='profile-pic'>
+                        <img></img>
+                        <p>Phuong Dang</p>
+                        </div>
                         <Link to ={`${path}`} onClick={this.hideMenu}>
                             Dash Home</Link>
 						<Link to ={`${path}/messages`}onClick={this.hideMenu}>
@@ -65,22 +62,30 @@ class Dashboard extends Component{
 						<Link to ={`${path}/upload`} onClick={this.hideMenu}>
                             Upload Images</Link>
 					</nav>
-				
-					<Switch>
-                        <Route exact path= {`${path}`}
-                             component = {DashHome} />
-						<Route exact path= {`${path}/messages`}
-                             component ={Messages}/>
-						<Route exact path= {`${path}/blog`}
-                             component ={PostBlog}/>
-						<Route exact path= {`${path}/upload`} 
-                             component = {UploadTest}/>
-					</Switch>
+
+                    <div className='dash-view'>
+                        <nav className='dash-topnav'>
+                            <a className='hamburger' onClick={this.showMenu}></a>
+                            <h1 className='flex-space title'>Volcano Boyz</h1> 
+                            <a className='home-icon'onClick={this.goHome} ></a> 
+                        </nav>
+                        <Switch>
+                            <Route exact path= {`${path}`}
+                                 component = {DashHome} />
+                            <Route exact path= {`${path}/messages`}
+                                 component ={Messages}/>
+                            <Route exact path= {`${path}/blog`}
+                                 component ={PostBlog}/>
+                            <Route exact path= {`${path}/upload`} 
+                                 component = {UploadTest}/>
+                        </Switch>
                     </div>
+				
 				</div>
 			</Router>
 		)
 	}	
 }
+
 export default Dashboard
 
