aaea3993346c8be13f62a16705c4a126cbede3f6
lesteven <le_steven@outlook.com>
2018-01-08 12:49:39 -0800

    add oneEditor media

--- src/client/adminContainers/PostBlogContainer.jsx
+++ src/client/adminContainers/PostBlogContainer.jsx
@@ -1,18 +1,99 @@
 import React,{Component} from 'react';
+import {AtomicBlockUtils, 
+        Editor, 
+        EditorState, 
+        RichUtils} from 'draft-js';
 import { connect } from 'react-redux';
-import {updateOne} from '../redux/modules/oneEditorModule';
+import {updateOne,
+        updateImage,
+        updateYT} from '../redux/modules/oneEditorModule';
 import RichEditor from '../adminComp/RichEditor.jsx';
+import BlockStyleControls from '../adminComp/BlockStyleControls.jsx';
+import InlineStyleControls from '../adminComp/InlineStyleControls.jsx';
+import TextInput from '../adminComp/TextInput.jsx';
+
 
 
 class PostBlogContainer extends Component {
+    toggleBlockType=(blockType)=> {
+      this.props.updateOne(
+        RichUtils.toggleBlockType(
+          this.props.oneEditor.editor,
+          blockType
+        )
+      );
+    }
+    toggleInlineStyle=(inlineStyle)=>{
+      this.props.updateOne(
+        RichUtils.toggleInlineStyle(
+          this.props.oneEditor.editor,
+          inlineStyle
+        )
+      );
+    }
+    confirmMedia=(type,content)=>{
+        const {editor} = this.props.oneEditor;
+//        console.log('confirm!',type,content);
+        const contentState = editor.getCurrentContent();
+        const contentStateWithEntity = contentState.createEntity(
+            type,
+            'IMMUTABLE',
+            {src: content}
+        );
+        const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+        const newEditorState = EditorState.set(
+            editor,
+            {currentContent: contentStateWithEntity}
+            );
+        const newReduxState = AtomicBlockUtils.insertAtomicBlock(
+            newEditorState,
+            entityKey,
+            ' '
+        )    
+        this.props.updateOne(newReduxState);
+    }
+    addImage=(blog)=>{
+        const content = this.props.oneEditor.imgURL;
+        this.confirmMedia('image',content);
+    }
+    addYouTube=(blog)=>{
+        const youtube = this.props.oneEditor.youtube;
+        const vidURL = 'https://www.youtube.com/embed/' 
+                + youtube.split('com\/watch?v=')[1];
+        const check = vidURL.split('&');
+        console.log(check);
+        this.confirmMedia('youtube',check[0]);
+    }
     render() {
-    const {editor} = this.props.oneEditor;
+    const {editor, youtube, imgURL} = this.props.oneEditor;
+    console.log(youtube, imgURL);
+    const blog = {_id:0};
         return (
             <div className='dash-container'>
-                <RichEditor
-                    editor = {editor}
-                    onChange = {this.props.updateOne}  
-                />
+                <div className='RichEditor-root'>
+                <BlockStyleControls editorState={editor}
+                    onToggle={this.toggleBlockType}/>
+                <InlineStyleControls editorState={editor}
+                    onToggle={this.toggleInlineStyle}/>
+                <TextInput 
+                    updateInput ={this.props.updateImage}
+                    inputValue ={imgURL}
+                    data ={blog}
+                    onSubmit={this.addImage}
+                    buttonText='Add Image'
+                    />
+                <TextInput
+                    updateInput ={this.props.updateYT}
+                    inputValue ={youtube}
+                    data ={blog}
+                    onSubmit={this.addYouTube}
+                    buttonText='Add Video'
+                    />
+                    <RichEditor
+                        editor = {editor}
+                        onChange = {this.props.updateOne}  
+                    />
+                </div>
             </div>
         )
     }
@@ -27,7 +108,9 @@ const mapStateToProps = (state) => {
 }
 const mapDispatchToProps = (dispatch) => {
     return{
-        updateOne: (editor) => dispatch(updateOne(editor))
+        updateOne: (editor) => dispatch(updateOne(editor)),
+        updateImage:(id,input)=>dispatch(updateImage(id,input)),
+        updateYT:(id,input)=>dispatch(updateYT(id,input))
 
     }
 }
