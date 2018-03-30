401d40771a3431487ba5ab5f2ffc82cfae50e0ce
Steven <stevenle2011@gmail.com>
2018-03-29 18:27:14 -0700

    update log strategy

--- src/server/auth/logStrategy.js
+++ src/server/auth/logStrategy.js
@@ -11,18 +11,12 @@ module.exports = function(passport,res){
 				//console.log(req.user)
 				if(err){
 					//console.log('error')
-					return res.json(err);}
-				if(!user){
-					//console.log('no user')
-					//console.log(req.body)
-					//console.log(done)
-					return res.json({err:'User does not exist.'})		
-				}
-				if(!user.validPassword(password)){
-					//console.log('pw not valid')
-					//res.json({err:'Incorrect password.'})
-					return res.json({err:'Invalid password'})
-				}
+					return res.json(err);
+        }
+
+        if (!user || !user.validPassword(password)) {
+          return res.json({err: 'Invalid ID or password'});
+        }
 				//console.log('done')
 				req.login(user,function(err){
 					if(err){return next(err)}
